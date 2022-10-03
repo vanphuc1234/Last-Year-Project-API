c03e89bb9f9c620c634884a58b59a7bd83432a29
thangka2207 <phamquocthang2207@gmail.com>
2022-10-04 04:15:23 +0700

    Add api sreachs by (area + direction + beds_count + baths_count)

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,11 @@ class Product < ApplicationRecord
         title: Faker::Address.full_address, 
         description: Faker::Address.full_address, 
         price: rand(1000000..5000000),
-        area: rand(50..100)
+        area: rand(50..100),
+        area: rand(50..100),
+        direction: ['huong_dong', 'huong_tay'].sample,
+        beds_count: rand(1..10),
+        baths_count: rand(1..10)
       )
     end
   end
